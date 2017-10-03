5e434a41375ce5de574e925de05007b193d90390
Alfredo G Ceballos <agceballos@cpp.edu>
2017-10-03 11:44:13 -0700

    Initial commit

---
+++ ChatClient.java
@@ -0,0 +1,19 @@
+/*****************************************
+*	Alfredo Ceballos
+*	CS 380 - Computer Networks
+*	Project 1
+*	Professor Nima Davarpanah
+*****************************************/
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ChatClient {
+	public static void main(String[] args) {
+		
+	}
+}
