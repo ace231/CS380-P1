dc71e97ca550684de0dade4a536d3f189795bb5e
Alfredo G Ceballos <agceballos@cpp.edu>
2017-10-04 17:23:05 -0700

    Got some stuff done

--- ChatClient.java
+++ ChatClient.java
@@ -13,7 +13,22 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ChatClient {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws Exception{
+		BufferedReader kb = new BufferedReader(new InputStreamReader(System.in));
+		while(true) {
+			try (Socket socket = new Socket("18.221.102.182", 1000)) {
+				String address = socket.getInetAddress().getHostAddress();
+				InputStream is = socket.getInputStream();
+				InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+				BufferedReader br = new BufferedReader(isr);
+				System.out.println(br.readLine());
+					
+					OutputStream os = socket.getOutputStream();
+					PrintStream out = new PrintStream(os, true, "UTF-8");
+			} catch(Exception e) {
+				System.out.println("Something went wrong...");
+				e.printStackTrace();
+			}		
+		}
 	}
 }
