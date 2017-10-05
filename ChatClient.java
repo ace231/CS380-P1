6b8cebfe642ebb560a5210e202bb746a5189263f
Alfredo G Ceballos <agceballos@cpp.edu>
2017-10-05 12:08:46 -0700

    Message exchanging works

--- ChatClient.java
+++ ChatClient.java
@@ -15,26 +15,36 @@ import java.net.Socket;
 public class ChatClient {
 	public static void main(String[] args) throws Exception{
 		BufferedReader kb = new BufferedReader(new InputStreamReader(System.in));
-		Socket socket = new Socket("18.221.102.182", 38001);
+		String msg = "";
 		
-		// Checking that connection went through
-		String address = socket.getInetAddress().getHostAddress();
-		System.out.printf("Connected to %s", address);
+		try(Socket socket = new Socket("18.221.102.182", 38001)) {
 		
-		// Creating client input stream to receive messages from server	
-		InputStream is = socket.getInputStream();
-		InputStreamReader isr = new InputStreamReader(is, "UTF-8");
-		BufferedReader br = new BufferedReader(isr);
-		
-		// Creating client output stream to send messages to server
-		OutputStream os = socket.getOutputStream();
-		PrintStream out = new PrintStream(os, true, "UTF-8");
-		
-		System.out.print("Username: ");
-		String uName = kb.readLine();
-		out.println(uName);
-		
-		System.out.println(br.readLine());
-
-	}
+			// Checking that connection went through
+			String address = socket.getInetAddress().getHostAddress();
+			System.out.printf("Connected to %s%n", address);
+			
+			// Creating client input stream to receive messages from server	
+			InputStream is = socket.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+			BufferedReader br = new BufferedReader(isr);
+			
+			// Creating client output stream to send messages to server
+			OutputStream os = socket.getOutputStream();
+			PrintStream out = new PrintStream(os, true, "UTF-8");
+			
+			// Asking user to input username
+			System.out.print("Username: ");
+			String uName = kb.readLine();
+			out.printf("%s%n", uName);	// Sending name to server
+			
+			// Loop handles new messages from client to server and from
+			// server to client
+			while(true) {
+				System.out.print("Message: ");
+				uName = kb.readLine();
+				out.printf("%s%n", uName);
+				System.out.println(br.readLine());
+			}
+		}// End or try
+	}// End of main
 }
